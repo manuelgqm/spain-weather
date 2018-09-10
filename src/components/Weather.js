831922b09e86411639041d44bef44cc5d761d1fa
Manuel García de Quirós Moreno <manuelgqm@gmail.com>
2018-09-10 16:54:49 +0200

    adds weather fetch updtates and more wheather data info fields

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,37 +1,52 @@
-import {React, Component} from "react";
+import React, {Component} from "react";
 
 class Weather extends Component {
-  constructor (props) {
+  constructor( props ) {
     super(props);
     this.state = {
-      items: [],
+      weather: [],
+      main: {},
       isLoaded: false,
       error: null
     };
   }
 
-  componentDidMount(props) {
+  componentDidMount() {
+    this.fetchWeather(this.props.match.params.id);
+  }
+
+  componentWillReceiveProps(nextProps){
+    if (this.props.match.url !== nextProps.match.url) {
+      this.setState({
+        isLoaded: false
+      });
+      this.fetchWeather(nextProps.match.params.id);
+    }
+  }
+
+  fetchWeather(locationId) {
     const appid = "bb2e07ba2ba01365ec144453ea7ddfb5";
-    fetch('http://api.openweathermap.org/data/2.5/weather?id=' + this.props.selectedLocation.id + '&appid=' + appid)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.weather
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          })
-        }
-      );
+    fetch('http://api.openweathermap.org/data/2.5/weather?id=' + locationId + '&appid=' + appid)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          weather: result.weather,
+          main: result.main
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      }
+    );
   }
 
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { error, isLoaded, weather, main } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -39,11 +54,14 @@ class Weather extends Component {
     } else {
       return (
         <ul>
-          {items.map(item => (
+          {weather.map(item => (
             <li key={item.id}>
-              {item.main} {item.description}
+              The weather is: {item.main}
             </li>
           ))}
+          <li>Temperature: {main.temp}, Min: {main.temp_min}, Max: {main.temp_max}</li>
+          <li>Pressure: {main.pressure}</li>
+          <li>Humidity: {main.humidity}</li>
         </ul>
       );
     }
